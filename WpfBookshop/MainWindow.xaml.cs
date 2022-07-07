1c83c519343b1b36d57854da1fea2216b12efc4b
PWidla <patrykwidlazb@gmail.com>
2022-07-07 18:51:33 +0200

    Main windows show data (books from db)

--- WpfBookshop/MainWindow.xaml.cs
+++ WpfBookshop/MainWindow.xaml.cs
@@ -21,31 +21,36 @@ namespace WpfBookshop
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<book> BooksList { get; set; }
+        private void DG_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            string headername = e.Column.Header.ToString();
+
+            //Cancel the column you don't want to generate
+            if (headername == "wishlists")
+            {
+                e.Cancel = true;
+            }
+            if (headername == "bookID")
+            {
+                // e.Column.Header = "First Name";
+                e.Cancel = true;
+            }
+           
+        }
+
         public MainWindow()
         {
             InitializeComponent();
 
-
+            
 
             using (BOOKSHOPEntities context = new BOOKSHOPEntities())
             {
-                //  Your programmatically created DataGrid is attached to MainGrid here
-                var dg = new DataGrid();
-                this.BooksGrid.Children.Add(dg);
-
-                // create four columns here with same names as the DataItem's properties
-                for (int i = 1; i <= 4; ++i)
-                {
-                    var column = new DataGridTextColumn();
-                    column.Header = "Column" + i;
-                    column.Binding = new Binding("Column" + i);
-                    dg.Columns.Add(column);
-                }
-
-                // create and add two lines of fake data to be displayed, here
-                dg.Items.Add(new DataItem { Column1 = "a.1", Column2 = "a.2", Column3 = "a.3", Column4 = "a.4" });
-                dg.Items.Add(new DataItem { Column1 = "b.1", Column2 = "b.2", Column3 = "b.3", Column4 = "b.4" });
+                BooksList = context.books.ToList();
             }
+            BooksGrid.ItemsSource = BooksList;
         }
     }
 }
+
